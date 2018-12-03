ef65a99e1afb04a2fb6e3345c43ca7942a4a5d6f
duynhat <duynhat240896@gmail.com>
2018-12-03 23:41:22 +0700

    Update menu.

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,14 +8,14 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'content_paste', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
+    { path: '/dashboard', title: 'Trang chủ',  icon: 'dashboard', class: '' },
+    { path: '/table-list', title: 'Xem traffic',  icon:'content_paste', class: '' },
+    { path: '/typography', title: 'Cấu hình log',  icon:'library_books', class: '' },
+    { path: '/notifications', title: 'Thông báo',  icon:'notifications', class: '' },
+    { path: '/user-profile', title: 'Cài đặt',  icon:'person', class: '' },
+    //{ path: '/icons', title: 'Icons',  icon:'bubble_chart', class: '' },
+    //{ path: '/maps', title: 'Maps',  icon:'location_on', class: '' },
+    //{ path: '/upgrade', title: 'Upgrade to PRO',  icon:'unarchive', class: 'active-pro' },
 ];
 
 @Component({
