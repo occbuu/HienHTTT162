923fd7d3c4081cbbc983739adc6a61ac2f5578f0
Hao Lee <occbuu>
2018-12-07 15:56:50 +0700

    add dblocal...

---
+++ src/app/snort-db.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class SnortDbService {
+  snortData:any;
+
+  
+  constructor() { 
+    this.loadSnortData().then((result) => {
+      let TAFFY = require( 'taffy' ).taffy;
+      console.log(result)
+      this.snortData = TAFFY.taffy(result);
+    })
+  }
+
+  loadSnortData(): Promise<any> {
+    return new Promise((resolve) => {
+      var xobj = new XMLHttpRequest();
+      xobj.overrideMimeType("application/json");
+      xobj.open('GET', 'assets/snortdata.json', true);
+      xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == 200) {
+          resolve(xobj.responseText);
+        }
+      };
+      xobj.send(null);
+    })
+  }
+
+  findMultiObjects(dataList: Array<{}>): Promise<any> {
+    return new Promise((resolve) => {
+      var wl = [];
+      dataList.forEach(word => {
+        wl.push(this.snortData({ word: word }).first())
+      });
+      resolve(wl)
+    })
+  }
+
+  findOneObject(word: string) {
+    var fword = this.snortData({ word: word }).first()
+    return fword;
+  }
+
+}
